cf25ec30fc38e48cbffdf7b61af7d6588ea13a7a
Ibrahim Sabbir <sabbir@Ibrahim-Sabbir-MacBook-Pro.local>
2025-07-21 11:36:08 +0600

    Post Working

--- framework/package.js
+++ framework/package.js
@@ -1,8 +1,16 @@
 const json = (req, res, next) => {
     let data = ``;
     req.on("data", chunk => data += chunk)
-    req.on("end", () => console.log("Data End"))
-    req.on("error", err => console.log(`Error from ${err}`))
+    req.on("end", () => {
+        try {
+            req.body = JSON.parse(data);
+            next();
+        } catch (err) {
+            res.statusCode = 400;
+            res.end('Invalid JSON');
+        }
+        console.log(res.body);
+    })
 }
 
 
--- index.js
+++ index.js
@@ -1,35 +1,48 @@
 const { log } = require("console");
 const MiniExpress = require("./framework/main.js");
+const json = require("./framework/package.js");
 const app = new MiniExpress;
 const port = 3000;
 
 app.use((req, res, next) => {
-    log(`Request received from this URL: ${req.url}`);
-    next(); // Call the next middleware or route handler
+    log(`Request received from this URL: ${req.url}\n\n`);
+    next();
 });
 
 app.use((req, res, next) => {
     log(`Lol, is it working?`);
-    next(); // Call the next middleware or route handler
+    next();
 });
 
 app.use((req, res, next) => {
-    log(`Lol, maybe it is working?`);
-    // next(); // Call the next middleware or route handler
+    log(`Lol, maybe it is working?\n\n`);
+    next();
 });
 
 // Get routes
 app.get("/data", (req, res) => {
     log("GET request to /data");
-    res.end("Hello, World!");
+    res.end("here is your data!");
 });
 app.get("/kata", (req, res) => {
     log("GET request to /mata");
-    res.end("Hello, World!");
+    res.end("Kata");
 });
 app.get("/sata", (req, res) => {
     log("GET request to /sata");
-    res.end("Hello, World!");
+    res.end("Sata");
+});
+
+app.use((req, res, next) => {
+    json(req, res, next);
+    log("JSON middleware executed");
+    next();
+});
+
+// Post routes
+app.post("/data", (req, res) => {
+    log("POST request to /data");
+    res.end("Data received!");
 });
 
 app.listhen(port, () => {
